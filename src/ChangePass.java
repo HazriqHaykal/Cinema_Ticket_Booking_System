09cc968f5159cd4e9e16c81680dcb0dc1658a79c
Firrrd <ammarfirdaus15@gmail.com>
2026-01-17 23:20:48 +0800

    Update comment on ChangePass class

--- src/ChangePass.java
+++ src/ChangePass.java
@@ -15,19 +15,19 @@ public class ChangePass {
     JPasswordField newPass;
     JButton confirm;
     JButton cancel;
-    JLabel phoneLbl;
-    JTextField phoneTxt;
+    JLabel phoneLbl; // modification
+    JTextField phoneTxt; // modification
 
     public ChangePass() {
         initGUI();
     }
 
     public void initGUI() {
-        miniFrame = new JFrame("Update Profile");
+        miniFrame = new JFrame("Update Profile"); // modification
         oldPassLbl = new JLabel("Your current password:");
         newPassLbl = new JLabel("Your new password:");
-        phoneLbl = new JLabel("Update Phone #:");
-        phoneTxt = new JTextField();
+        phoneLbl = new JLabel("Update Phone #:"); // modification
+        phoneTxt = new JTextField(); // modification
         oldPass = new JPasswordField();
         newPass = new JPasswordField();
         confirm = new JButton("Confirm");
@@ -37,17 +37,17 @@ public class ChangePass {
         newPassLbl.setBounds(10, 80, 150, 30);
         oldPass.setBounds(160, 30, 300, 30);
         newPass.setBounds(160, 80, 300, 30);
-        phoneLbl.setBounds(10, 130, 150, 30);
-        phoneTxt.setBounds(160, 130, 300, 30);
-        cancel.setBounds(240, 180, 100, 30);
+        phoneLbl.setBounds(10, 130, 150, 30); // modification
+        phoneTxt.setBounds(160, 130, 300, 30); // modification
+        cancel.setBounds(240, 180, 100, 30); // modification
         cancel.setBackground(Color.black);
         cancel.setForeground(Color.WHITE);
-        confirm.setBounds(360, 180, 100, 30);
+        confirm.setBounds(360, 180, 100, 30); // modification
         confirm.setBackground(Color.black);
         confirm.setForeground(Color.WHITE);
 
-        miniFrame.add(phoneLbl); // Add to frame
-        miniFrame.add(phoneTxt);
+        miniFrame.add(phoneLbl); // modification
+        miniFrame.add(phoneTxt); // modification
         miniFrame.add(oldPassLbl);
         miniFrame.add(oldPass);
         miniFrame.add(newPassLbl);
@@ -57,7 +57,7 @@ public class ChangePass {
 
         miniFrame.setLayout(null);
         miniFrame.setResizable(false);
-        miniFrame.setSize(500, 300);
+        miniFrame.setSize(500, 300); // modification
         miniFrame.setVisible(true);
     }
 }
